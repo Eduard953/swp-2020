1dc1e25ddacfc1524677794cf241a407b02b2acb
benn1S <39101696+benn1S@users.noreply.github.com>
2020-06-29 15:49:03 +0200

    includes test and train split

--- dataset_prep.py
+++ dataset_prep.py
@@ -4,7 +4,9 @@
 
 
 Software Projekt/
+│
 ├── BeesBook/
+│   │
 │   ├── 0/
 │   │     ├── ann/
 │   │     │    └── jsonfiles...
@@ -15,10 +17,23 @@ Software Projekt/
 │         │    └── jsonfiles...
 │         └── img/
 │              └── images...
-    
-        # 3, 4 etc. for each batch of annotated imgs from supervisly
-
+│    
+│        # 3, 4 etc. for each batch of annotated imgs from supervisly
+│
 ├── ground_truths/
+│    │
+│    ├──training_images/
+│    │    │
+│    │    ├── 0 notpollen/
+│    │    │
+│    │    └── 1 pollen/
+│    │    
+│    └───test_images/
+│         │
+│         ├── 0 notpollen/
+│         │
+│         └── 1 pollen/
+│
 └── dataset_prep.py
 
 
@@ -39,112 +54,126 @@ import os
 import json
 from PIL import Image
 import random
-import csv
-
 
 # global vars
 
 crop_size = 84 / 2
 number_of_random_crops = 10
 img_shape = (4000,3000)
-dir_target = "ground_truths/"
+dir_target_training_0 = "ground_truths/training_images/0 notpollen/"
+dir_target_training_1 = "ground_truths/training_images/1 pollen/"
+dir_target_test_0 = "ground_truths/test_images/0 notpollen/"
+dir_target_test_1 = "ground_truths/test_images/1 pollen/"
 
 
 #loop for each folder with annotated images 
 
 for x in range(len(next(os.walk('BeesBook/'))[1])):
     
-    
-    #write a csv file
 
-    with open('ground_truths/Aforagers.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-    
-        # folder dir, edit for your pc?
+    # folder dir, edit for your pc?
 
-        dir_json = "BeesBook/" + str(x) + "/ann/"
-        dir_img = "BeesBook/" + str(x) + "/img/"
+    dir_json = "BeesBook/" + str(x) + "/ann/"
+    dir_img = "BeesBook/" + str(x) + "/img/"
 
 
-        # list of json/img
+    # list of json/img
 
-        list_json = os.listdir(dir_json)
-        list_img = os.listdir(dir_img)
+    list_json = os.listdir(dir_json)
+    list_img = os.listdir(dir_img)
   
-        #loop through folder
+    #loop through folder
       
-        for b in range(len(list_img)):
+    for b in range(len(list_img)):
 
     
-            # load json file
+        # load json file
     
-            data = json.load(open(dir_json + list_json[b],))
+        data = json.load(open(dir_json + list_json[b],))
 
     
-            # number of marked points
+        # number of marked points
         
-            data_len = len(data['objects'])
-            coords = []
+        data_len = len(data['objects'])
+        coords = []
 
 
         # if points in image, crop it, save it for GT1
 
     
-            if data_len > 0:
+        if data_len > 0:
     
-                for a in range(data_len):
-                    for i in data['objects'][a]['points']['exterior']: 
-                        coords.append(i)
+            for a in range(data_len):
+                for i in data['objects'][a]['points']['exterior']: 
+                    coords.append(i)
             
             
-                    # select current image
+                # select current image
 
-                    current_img = Image.open(dir_img + list_img[b])
+                current_img = Image.open(dir_img + list_img[b])
     
-                    x1 = coords[a][0] - crop_size
-                    y1 = coords[a][1] - crop_size
-                    x2 = coords[a][0] + crop_size
-                    y2 = coords[a][1] + crop_size
+                x1 = coords[a][0] - crop_size
+                y1 = coords[a][1] - crop_size
+                x2 = coords[a][0] + crop_size
+                y2 = coords[a][1] + crop_size
         
                 
-                    #save img
+                #save img
                 
-                    current_img = current_img.crop((x1,y1,x2,y2))
-                    current_img.save( dir_target + "GT_1_img_" + str(x) + '_' + str(b) + '_' + str(a) + ".png")
-                
-                
-                    #write into csv file with ,1
-                
-                    writer.writerow(["GT_1_img_" + str(x) + '_' + str(b) + '_' + str(a) + ".png", 1])
+                current_img = current_img.crop((x1,y1,x2,y2))
+                current_img.save( dir_target_training_1 + "GT_1_img_" + str(x) + '_' + str(b) + '_' + str(a) + ".png")
          
     
             # if no points in image, random crops, save it for GT0
         
-            elif data_len == 0:
+        elif data_len == 0:
         
             
-                for t in range(number_of_random_crops):
+            for t in range(number_of_random_crops):
                 
                             
-                    # select current image
+                # select current image
     
-                    current_img = Image.open(dir_img + list_img[b])
+                current_img = Image.open(dir_img + list_img[b])
         
         
-                    rnd_coord = (random.randint(crop_size,img_shape[0]-crop_size),random.randint(crop_size,img_shape[1]-crop_size))
+                rnd_coord = (random.randint(crop_size,img_shape[0]-crop_size),random.randint(crop_size,img_shape[1]-crop_size))
                 
-                    x1 = rnd_coord[0] - crop_size
-                    y1 = rnd_coord[1] - crop_size
-                    x2 = rnd_coord[0] + crop_size
-                    y2 = rnd_coord[1] + crop_size
+                x1 = rnd_coord[0] - crop_size
+                y1 = rnd_coord[1] - crop_size
+                x2 = rnd_coord[0] + crop_size
+                y2 = rnd_coord[1] + crop_size
         
         
-                    #save img
+                #save img
         
-                    current_img = current_img.crop((x1,y1,x2,y2))
-                    current_img.save( dir_target + "GT_0_img_" + str(x) + '_' + str(b) + '_' + str(t) + ".png")
-                    
-                    #write into csv file with ,0
-                    
-                    writer.writerow(["GT_0_img_" + str(x) + '_' + str(b) + '_' + str(t) + ".png", 0])
+                current_img = current_img.crop((x1,y1,x2,y2))
+                current_img.save( dir_target_training_0 + "GT_0_img_" + str(x) + '_' + str(b) + '_' + str(t) + ".png")
             
+#spit test and training
+
+c = next(os.walk(dir_target_training_0))[2]
+c_o_dot_len = int(0.2 * len(c))
+    
+for y in range(c_o_dot_len):
+    
+    c = next(os.walk(dir_target_training_0))[2]
+    c_len = len(c)
+    c_dot_len = int(0.2 * len(c))
+
+    rnd = random.randint(0,c_len -1)
+    os.rename(dir_target_training_0 + str(c[rnd]), dir_target_test_0 + str(c[rnd]))
+    
+    
+
+c = next(os.walk(dir_target_training_1))[2]
+c_o_dot_len = int(0.2 * len(c)) 
+
+for y in range(c_o_dot_len):
+    
+    c = next(os.walk(dir_target_training_1))[2]
+    c_len = len(c)
+    c_dot_len = int(0.2 * len(c))
+
+    rnd = random.randint(0,c_len -1)
+    os.rename(dir_target_training_1 + str(c[rnd]), dir_target_test_1 + str(c[rnd]))
