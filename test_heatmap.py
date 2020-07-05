d16ac19e092cc8fe987f562e679b2779ef0428f9
bumblebee2 <sophie.zabel@gmx.de>
2020-07-05 23:13:41 +0200

    include plots

--- test_heatmap.py
+++ test_heatmap.py
@@ -4,19 +4,76 @@ import matplotlib.pyplot as plt
 import matplotlib.transforms as mtransforms
 import numpy as np
 from main import *
-from torchvision import transforms
 import skimage.io, skimage.transform
-from skimage.feature import peak_local_max
 from skimage.color import rgb2gray
 from scipy import ndimage as ndi
-from PIL import Image, ImageDraw
-from scipy.spatial.kdtree import KDTree
-import plot
+from PIL import Image
 
 def get_orig_position(liste):
-    liste[:, 0] = (liste[:, 0] *48) + 24
-    liste[:, 1] = (liste[:, 1] * 48) + 24
+    liste[:, 0] = ((liste[:, 0] + 0.5) * 48) - 0.5 + 24
+    liste[:, 1] = ((liste[:, 1] + 0.5) * 48) - 0.5 + 24
     return liste
+
+
+def plot_heatgraph(number_of_epochs, epochs, train_loss, precision, recall, f1_score, date_time):
+    f, ax = plt.subplots(1, figsize = (10,5))
+    plt.xlabel('epochs')
+    plt.ylabel('train loss, precision, recall, F1 score')
+    ax.plot(epochs, train_loss, 'r', label = 'Train loss')
+    ax.plot(epochs, precision, 'g', label = 'Precision')
+    ax.plot(epochs, recall, 'b', label = 'Recall')
+    ax.plot(epochs, f1_score, 'm', label = 'F1 score')
+    plt.axis([0, (number_of_epochs - 1), 0, 1])
+    ax.set_ylim(bottom=0)
+    ax.set_xticks(np.arange(0, number_of_epochs, (number_of_epochs // 10)))
+    ax.set_yticks(np.arange(0, 1.1, 0.1))
+    ax.tick_params(axis='x', direction='inout')
+    ax.tick_params(axis='y', direction='inout')
+    leg = ax.legend(bbox_to_anchor=(1.01, 0.5), loc = 'center left', frameon = True)
+    leg.get_frame().set_edgecolor('k')
+    plt.grid(False)
+    plt.tight_layout()
+    plt.savefig('../Plot/heatgraph_{}.svg'.format(date_time.strftime('%d-%m-%y_%H:%M')), dpi = 100)
+
+
+def plot_heatmap(img_name, epoch, original_image, output_greater, list_label, list_max, date_time):
+    width_origimg = original_image.shape[1]
+    height_origimg = original_image.shape[0]
+    print('{}x{}'.format(width_origimg, height_origimg))
+    width_out = output_greater.shape[1]
+    height_out = output_greater.shape[0]
+    print('{}x{}'.format(width_out, height_out))
+
+    extent = [0, width_origimg, height_origimg, 0]
+
+    fig, ax = plt.subplots(figsize=(20,10))
+    ax.imshow(original_image, cmap='gray', extent=extent)
+    heatmap = ax.imshow(output_greater, interpolation='bicubic', alpha=0.4, extent=extent, cmap='viridis')
+    trans_heatmap = mtransforms.Affine2D().scale((4000-24)/4000, (3000-24)/3000).translate(24,24) + ax.transData
+    heatmap.set_transform(trans_heatmap)
+    ax.axis('off')
+    cbar = ax.figure.colorbar(heatmap, ax=ax)
+    cbar.ax.set_ylabel(None, rotation=-90, va='bottom')
+    plt.savefig('../Heatmap/{}_heatmap_overlay_{}_{}'.format(img_name, date_time.strftime('%d-%m-%y_%H:%M'), epoch), bbox_inches='tight')
+    plt.close(fig)
+
+
+def plot_coordinates(img_name, epoch, original_image, list_label, list_max, date_time):
+    fig, ax =  plt.subplots(figsize=(20, 10))
+    ax.imshow(original_image, cmap='gray')
+    ax.scatter(list_label[:, 0], list_label[:, 1], marker='+')
+    ax.scatter(list_max[:, 0], list_max[:, 1], marker='.')
+    plt.axis('off')
+    plt.savefig('../Heatmap/{}_label_max_coordinates_{}_{}'.format(img_name, date_time.strftime('%d-%m-%y_%H:%M'), epoch), bbox_inches='tight')
+    plt.close(fig)
+
+
+def create_heatmaps(img_name, epoch, original_image, output_greater, image_max, mask, list_label, list_max, net_coord, date_time):
+    # heatmap overlaid onto raw image
+    plot_heatmap(img_name, epoch, original_image, output_greater, list_label, list_max, date_time)
+
+    # compare points of list_label to points of list_max
+    plot_coordinates(img_name, epoch, original_image, list_label, list_max, date_time)
 
 
 def create_score(img_name, epoch, list_label, list_max, num_pred, true_positives, false_positives, false_negatives):
@@ -74,7 +131,7 @@ def create_coord_lists(number_of_epochs, json_filename, image_filename, img_name
 
     image_max = ndi.maximum_filter(output_greater.copy(), size=8, mode='constant')
     mask = output_greater == image_max
-    mask &= output_greater > 0.96
+    mask &= output_greater > 0.50
 
     net_coord = np.nonzero(mask)
     net_coord = np.fliplr(np.column_stack(net_coord))
@@ -88,6 +145,9 @@ def create_coord_lists(number_of_epochs, json_filename, image_filename, img_name
     
     print('*** list_max (maxima of net_coord) ***\n{}\n*** list_label (marked bees) ***\n{}'.format(list_max, list_label))
 
+    if (list_max.size != 0 and epoch == number_of_epochs-1):
+        create_heatmaps(img_name, epoch, original_image, output_greater, image_max, mask, list_label, list_max, net_coord, date_time)
+    
     num_pred, true_positives, false_positives, false_negatives = create_score(img_name, epoch, list_label, list_max, num_pred, true_positives, false_positives, false_negatives)
 
     return num_pred, true_positives, false_positives, false_negatives
@@ -149,3 +209,4 @@ def loop_epochs(given_arguments, net, date_time, number_of_epochs):
 
 def heatmap(given_arguments, net, date_time, number_of_epochs):
     precision, recall, f1_score = loop_epochs(given_arguments, net, date_time, number_of_epochs)
+    plot_heatgraph(number_of_epochs, epochs, train_loss, precision, recall, f1_score, date_time)
